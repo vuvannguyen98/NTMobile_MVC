3a46b72c7e6c7a1e3005c66ed79030d4eb942fee
Nguyên Vũ Văn <hoangnguyenvuive210698@gmail.com>
2019-06-21 09:55:33 +0700

    lesson7 : Create solution

--- WebNTMobileApp/Data/ApplicationDbContext.cs
+++ WebNTMobileApp/Data/ApplicationDbContext.cs
@@ -20,7 +20,7 @@ namespace WebNTMobileApp.Data
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            // Add your customizations after calling base.OnModelCreating(builder);.
         }
     }
 }
